3ce43f5a6a4baa3a40c1f36c1549326b00043475
umesh <umeshbltr9@gmail.com>
2024-09-05 16:31:13 +0530

    cors policy updated

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ import bodyParser from "body-parser";
 
 import Routes from "./routes/routes.js";
 
+const corsOptions = {
+    origin: 'https://client-peach.vercel.app', // Replace with your client's domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // if your client needs to send cookies with the requests
+  };
 
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json({extended: true}));
 app.use(bodyParser.urlencoded({extended: true}));
 
